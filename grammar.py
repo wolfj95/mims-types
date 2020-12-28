3a0adba8e86a2d44eae816b98a004934a452ba2d
Chris Proctor <chris.proctor@gmail.com>
2020-12-28 18:53:12 -0500

    removing solutions

--- grammar.py
+++ grammar.py
@@ -15,6 +15,8 @@ from grammatical_types import (
     Sentence
 )
 
+# NEEDED FOR LOVE POEM
+
 def pluralize(noun):
     "Noun -> PluralNoun"
     assert type(noun) == Noun
@@ -23,56 +25,39 @@ def pluralize(noun):
     else:
         return PluralNoun(noun + 's')
 
-def past_tense_transitive(verb):
-    "TransitiveVerb -> PastTenseTransitiveVerb"
-    assert type(verb) == TransitiveVerb
-    if verb.endswith('e'):
-        return PastTenseTransitiveVerb(verb + 'd')
-    else:
-        return PastTenseTransitiveVerb(verb + 'ed')
-
-def past_tense_intransitive(verb):
-    "TransitiveVerb -> PastTenseTransitiveVerb"
-    assert type(verb) == IntransitiveVerb
-    if verb.endswith('e'):
-        return PastTenseIntransitiveVerb(verb + 'd')
-    else:
-        return PastTenseIntransitiveVerb(verb + 'ed')
+# NEEDED FOR COUPLET
 
 def noun_phrase(adjective, noun):
     "(Adjective, Noun) -> NounPhrase"
-    assert isinstance(adjective, Adjective)
-    assert isinstance(noun, NounPhrase)
-    return NounPhrase(adjective + ' ' + noun)
+    raise NotImplemented("This function isn't done!")
 
 def determine_noun_phrase(determiner, noun_phrase):
     "(Determiner, NounPhrase) -> Determined NounPhrase"
-    assert isinstance(determiner, Determiner)
-    assert isinstance(noun_phrase, NounPhrase)
-    assert not isinstance(noun_phrase, DeterminedNounPhrase)
-    return DeterminedNounPhrase(determiner + ' ' + noun_phrase)
+    raise NotImplemented("This function isn't done!")
 
 def make_definite(noun_phrase):
     "NounPhrase -> DeterminedNounPhrase"
-    article = Determiner("the")
-    return determine_noun_phrase(article, noun_phrase)
+    raise NotImplemented("This function isn't done!")
 
 def make_indefinite(noun_phrase):
     "NounPhrase -> DeterminedNounPhrase"
-    if starts_with_vowel_sound(noun_phrase):
-        article = Determiner("an")
-    else:
-        article = Determiner("a")
-    return determine_noun_phrase(article, noun_phrase)
+    raise NotImplemented("This function isn't done!")
 
-def verb_phrase_transitive(verb, noun_phrase):
-    "(TransitiveVerb, NounPhrase) -> VerbPhrase"
-    assert isinstance(verb, TransitiveVerb)
-    assert isinstance(noun_phrase, NounPhrase)
-    return VerbPhrase(verb + ' ' + noun_phrase)
+# NEEDED FOR LIMERICK
 
 def verb_phrase(adverb, verb_phrase):
     "(Adverb, VerbPhrase) -> VerbPhrase"
-    assert isinstance(adverb, Adverb)
-    assert isinstance(verb_phrase, VerbPhrase)
-    return VerbPhrase(adverb + " " + verb_phrase)
+    raise NotImplemented("This function isn't done!")
+
+def past_tense_transitive(verb):
+    "TransitiveVerb -> PastTenseTransitiveVerb"
+    raise NotImplemented("This function isn't done!")
+
+def past_tense_intransitive(verb):
+    "TransitiveVerb -> PastTenseTransitiveVerb"
+    raise NotImplemented("This function isn't done!")
+
+def verb_phrase_transitive(verb, noun_phrase):
+    "(TransitiveVerb, NounPhrase) -> VerbPhrase"
+    raise NotImplemented("This function isn't done!")
+
